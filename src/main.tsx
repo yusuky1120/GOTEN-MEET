05e37b7701a6155a391df23cf3dc2e00c8ae57b5
yusuky1120 <77489049ad@gmail.com>
2026-07-26 22:35:11 +0900

    fix: restore original frontend bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import { installTokenApiFetch } from './api/tokenApiFetch';
 import './styles.css';
-
-installTokenApiFetch();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
