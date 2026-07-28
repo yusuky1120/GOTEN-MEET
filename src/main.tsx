18301ba669e0d2f79fa33dded4e169d5f604e205
yusuky1120 <77489049ad@gmail.com>
2026-07-29 03:10:32 +0900

    style: load compact layout overrides

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import { installTokenApiFetch } from './api/tokenApiFetch';
 import './styles.css';
+import './ui-layout.css';
 
 installTokenApiFetch();
 
